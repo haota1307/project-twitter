1de4374cc267b1e5bbadde0527358a3c005bbe26
Tran Anh Hao <Trananhhao1307@gmail.com>
2023-12-30 20:44:30 +0700

    servideo stream

--- be-twitter/src/routes/static.routes.ts
+++ be-twitter/src/routes/static.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express'
-import { serveImageController } from '~/controllers/medias.controller'
+import { serveImageController, serveVideoStreamController } from '~/controllers/medias.controller'
 const staticRouter = Router()
 /** Hiển thị video và hình ảnh */
 
 staticRouter.get('/image/:name', serveImageController)
+staticRouter.get('/video-stream/:segment/:name', serveVideoStreamController)
 
 export default staticRouter
