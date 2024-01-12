153b7d971b2f7173c0d6c7e96507e67e33bd82aa
Tran Anh Hao <Trananhhao1307@gmail.com>
2024-01-12 14:21:47 +0700

    Setup FE

---
+++ fe-twitter/tailwind.config.js
@@ -0,0 +1,23 @@
+const plugins = require('tailwindcss/plugin')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  corePlugins: {
+    container: false // Xóa class container của tailwind
+  },
+  plugins: [
+    plugins(function ({ addComponents, theme }) {
+      // Tạo tại class container mới
+      addComponents({
+        '.container': {
+          maxWidth: theme('columns.7xl'),
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          paddingLeft: theme('spacing.4'),
+          paddingRight: theme('spacing.4')
+        }
+      })
+    })
+  ]
+}
--- fe-twitter/vite.config.ts
+++ fe-twitter/vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    port: 1307
+  },
+  css: {
+    devSourcemap: true
+  },
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, './src')
+    }
+  }
 })
