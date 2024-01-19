879378c43c3134a06bb05304d850d6d4891496fa
Tran Anh Hao <Trananhhao1307@gmail.com>
2024-01-19 19:20:58 +0700

    Update

--- fe-twitter/src/components/PostItem/PostItem.tsx
+++ fe-twitter/src/components/PostItem/PostItem.tsx
@@ -47,17 +47,17 @@ export default function PostItem({ data, key }: PostItemProps) {
             md:block
         '
             >
-              @{data.user_id}
+              @{data?.user_id}
             </span>
-            <span className='text-neutral-500 text-sm'>{formatDate(data.created_at)}</span>
+            <span className='text-neutral-500 text-sm'>{formatDate(data?.created_at)}</span>
           </div>
           <div className='text-black mt-1'>{data?.content || ' '}</div>
           <div className='bg-slate-100 h-80 mt-2 flex'>
-            {data.medias[0]?.type === MediaType.Image && (
+            {data?.medias[0]?.type === MediaType.Image && (
               <img className='object-cover h-80 w-full' src={data.medias[0]?.url}></img>
             )}
 
-            {data.medias[0]?.type === MediaType.Video && (
+            {data?.medias[0]?.type === MediaType.Video && (
               <video className='h-80 w-full' src={data.medias[0]?.url}></video>
             )}
           </div>
--- fe-twitter/src/pages/Home/Home.tsx
+++ fe-twitter/src/pages/Home/Home.tsx
@@ -1,11 +1,9 @@
 import Header from 'src/components/Header'
-import PostItem from 'src/components/PostItem'
 
 export default function Home() {
   return (
     <>
       <Header isHomePage />
-      <PostItem />
     </>
   )
 }
