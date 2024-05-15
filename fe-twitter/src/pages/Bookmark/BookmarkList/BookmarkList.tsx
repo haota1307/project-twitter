145eefd077d36337770eb76ae105cefd7e3db212
Tran Anh Hao <Trananhhao1307@gmail.com>
2024-05-15 14:05:20 +0700

    update

--- fe-twitter/src/pages/Bookmark/BookmarkList/BookmarkList.tsx
+++ fe-twitter/src/pages/Bookmark/BookmarkList/BookmarkList.tsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from 'react'
+import { IoBookmarkSharp } from 'react-icons/io5'
 import InfiniteScroll from 'react-infinite-scroll-component'
+import { toast } from 'react-toastify'
+import interactApi from 'src/apis/interact.api'
 import PostItem from 'src/components/PostItem'
+import SkeletonLoading from 'src/components/SkeletonLoading'
 import config from 'src/constants/config'
 import { TweetType } from 'src/types/tweet.type'
 import http from 'src/utils/http'
@@ -72,6 +76,45 @@ export default function BookmarkList({ userId }: BookmarkProps) {
     if (userId) fetchData()
   }, [userId])
 
+  const TweetDeleted = ({ id }: any) => {
+    const handleUnBookmarkByUser = async (tweetId: string) => {
+      await interactApi
+        .unbookmarkTweet(tweetId)
+        .then(() => {
+          toast.success('unbookmark successfully')
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    }
+    console.log(id)
+    return (
+      <>
+        <div className='border-b w-full'>
+          <div className='flex items-center'>
+            <h1 className='mx-6'>The tweet has been deleted</h1>
+            <span className='mr-2'>Unbookmark now</span>
+            <div
+              onClick={() => handleUnBookmarkByUser(id as string)}
+              className='flex flex-row items-center text-yellow-400 gap-2 cursor-pointer hover:text-yellow-400 hover:bg-yellow-50 rounded-full p-2 transform active:scale-50 transition-transform'
+            >
+              <IoBookmarkSharp size={20} />
+            </div>
+          </div>
+          <div className='w-full mt-1 p-6 space-y-4 divide-y divide-gray-200 rounded animate-pulse'>
+            <div className='flex items-center justify-between'>
+              <div>
+                <div className='h-2.5 bg-gray-300 rounded-full w-24 mb-2.5' />
+                <div className='w-32 h-2 bg-gray-200 rounded-full ' />
+              </div>
+              <div className='h-2.5 bg-gray-300 rounded-full w-12' />
+            </div>
+          </div>
+        </div>
+      </>
+    )
+  }
+
   if (data.length === 0)
     return (
       <>
@@ -88,12 +131,14 @@ export default function BookmarkList({ userId }: BookmarkProps) {
       loader={<h4>Loading...</h4>}
     >
       {data?.map((post: Record<string, any>) => {
-        if (post?.tweet[0]?.type === TweetType?.Tweet && post.tweet[0] !== undefined)
-          return (
-            <>
+        return (
+          <>
+            {post.tweet[0] === undefined && <TweetDeleted id={post._id as string} />}
+            {post?.tweet[0]?.type === TweetType?.Tweet && (
               <PostItem key={post.tweet[0]._id} data={post.tweet[0] as any} user={post.user[0]} />
-            </>
-          )
+            )}
+          </>
+        )
       })}
     </InfiniteScroll>
   )
